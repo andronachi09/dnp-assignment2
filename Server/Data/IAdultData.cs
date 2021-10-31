ce46990c1f45946c6fc04a2038be2fae8fecda29
gof3ck69 <sorryy647@gmail.com>
2021-10-31 17:18:40 +0100

    Add files

---
+++ Server/Data/IAdultData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Server.Models;
+
+namespace Server.Data
+{
+    public interface IAdultData
+    {
+        Task<IList<Adult>> GetAdults();
+        Task<Adult> AddAdult(Adult adult);
+        Task UpdateAdult(Adult adult);
+        Task DeleteAdult(Adult adult);
+        Adult GetId(int id);
+    }
+}
